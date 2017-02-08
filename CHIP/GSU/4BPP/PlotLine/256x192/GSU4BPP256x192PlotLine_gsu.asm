476c8bb55c0b9dcabb9550eb60f7ddb8771e7ad3
PeterLemon <krom@gbadev.org>
2017-02-08 12:55:07 +0000

    GSU Plot Line Optimize

--- CHIP/GSU/4BPP/PlotLine/256x192/GSU4BPP256x192PlotLine_gsu.asm
+++ CHIP/GSU/4BPP/PlotLine/256x192/GSU4BPP256x192PlotLine_gsu.asm
@@ -26,59 +26,58 @@ GSUStart:
   iwt r4, #191 // R4 = Y1
 
   with r5 ; sub r5 // R5 = 0
-  from r3 ; to r7 ; sub r1 // R7 = DX (X1 - X0)
+  with r3 ; sub r1 // R3 = DX (X1 - X0)
   bpl SXPos
   inc r5 // IF (X1 > X0), R5 (SX) = 1 (Delay Slot)
   dec r5 // IF (X1 < X0), R5 (SX) = -1
   dec r5 // R5 = -1
-  with r7 ; not // R7 ~= R7
-  inc r7 // R7 = ABS(DX)
+  with r3 ; not // R3 ~= R3
+  inc r3 // R3 = ABS(DX)
   SXPos:
 
   with r6 ; sub r6 // R6 = 0
-  from r4 ; to r8 ; sub r2 // R8 = DY (Y1 - Y0)
+  with r4 ; sub r2 // R4 = DY (Y1 - Y0)
   bpl SYPos
   inc r6 // IF (Y1 > Y0), R6 (SY) = 1 (Delay Slot)
   dec r6 // IF (Y1 < Y0), R6 (SY) = -1
   dec r6 // R6 = -1
-  with r8 ; not // R8 ~= R8
-  inc r8 // R8 = ABS(DY)
+  with r4 ; not // R4 ~= R4
+  inc r4 // R4 = ABS(DY)
   SYPos:
 
-  from r7 ; cmp r8 // Compare DX To DY
-  blt RunY
+  from r3 ; cmp r4 // Compare DX To DY
+  blt YMajor // IF (DX < DY) Y Major, Else X Major
+  plot // Plot Color (R1++) (Delay Slot)
 
-  from r7 ; to r4 ; lsr // IF (DX >= DY), R4 (X Error) = R7 (DX) / 2 (X Error = DX / 2)
-  move r12, r7 // R12 = Loop Count (DX)
-  inc r12 // R12++
+  from r3 ; lsr // IF (DX >= DY), R0 (X Error) = R3 (DX) / 2 (X Error = DX / 2)
+  move r12, r3 // R12 = Loop Count (DX)
   move r13, r15 // R13 = Loop Address
   // LoopX:
-    plot // Plot Color (R1++)
-    with r4 ; sub r8 // Subtract R8 (DY) From R4 (X Error) & Compare R4 (X Error) To Zero (X Error -= DY)
+    sub r4 // Subtract R4 (DY) From R0 (X Error) & Compare R4 (X Error) To Zero (X Error -= DY)
     bge XEnd
+    dec r1 // R1-- (Delay Slot)
     with r2 ; add r6 // IF (X Error < 0), Add R6 (SY) To R2 (Y0) (Y0 += SY)
-    with r4 ; add r7 // IF (X Error < 0), Add R7 (DX) To R4 (X Error) (X Error += DX)
+    add r3 // IF (X Error < 0), Add R3 (DX) To R0 (X Error) (X Error += DX)
     XEnd:
       with r1 ; add r5 // Add R5 (SX) To R1 (X0) (X0 += SX)
       loop // LoopX, IF (X0 == X1), Line End
-      dec r1 // R1-- (Delay Slot)
+      plot // Plot Color (R1++) (Delay Slot)
       bra LineEnd
 
-  RunY:
-  from r8 ; to r3 ; lsr // IF (DX < DY), R3 (Y Error) = R8 (DY) / 2 (Y Error = DY / 2)
-  move r12, r8 // R12 = Loop Count (DY)
-  inc r12 // R12++
+  YMajor:
+  from r4 ; lsr // IF (DX < DY), R0 (Y Error) = R4 (DY) / 2 (Y Error = DY / 2)
+  move r12, r4 // R12 = Loop Count (DY)
   move r13, r15 // R13 = Loop Address
   // LoopY:
-    plot // Plot Color (R1++)
-    with r3 ; sub r7 // Subtract R7 (DX) From R3 (Y Error) & Compare R1 (Y Error) To Zero
+    sub r3 // Subtract R3 (DX) From R0 (Y Error) & Compare R1 (Y Error) To Zero
     bge YEnd
+    dec r1 // R1-- (Delay Slot)
     with r1 ; add r5 // IF (Y Error < 0), Add R5 (SX) To R1 (X0) (X0 += SX)
-    with r3 ; add r8 // IF (Y Error < 0), Add R8 (DY) To R3 (Y Error) (Y Error += DY)
+    add r4 // IF (Y Error < 0), Add R4 (DY) To R0 (Y Error) (Y Error += DY)
     YEnd:
       with r2 ; add r6 // Add R6 (SY) To R2 (Y0) (Y0 += SY)
       loop // LoopY, IF (Y0 == Y1), Line End
-      dec r1 // R1-- (Delay Slot)
+      plot // Plot Color (R1++) (Delay Slot)
 
   LineEnd:
     rpix // Flush Pixel Cache
