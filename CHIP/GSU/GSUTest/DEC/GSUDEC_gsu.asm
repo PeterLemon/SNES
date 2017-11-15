b59ada46dccd2cf101bfd5450e1370fff0ea689e
PeterLemon <krom@gbadev.org>
2017-11-15 03:15:23 +0000

    GSU DEC Test

---
+++ CHIP/GSU/GSUTest/DEC/GSUDEC_gsu.asm
@@ -0,0 +1,187 @@
+// SNES GSU Test DEC (Decrement) demo (GSU Code) by krom (Peter Lemon):
+arch snes.gsu
+
+GSUStart:
+  ////////////////////////////
+  // DEC register
+  ////////////////////////////
+
+  iwt r0, #$0001 // R0 = $0001
+  dec r0 // R0--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r0, #$0000 // R0 = $0000
+  dec r0 // R0--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r1, #$0001 // R1 = $0001
+  dec r1 // R1--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r1, #$0000 // R1 = $0000
+  dec r1 // R1--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r2, #$0001 // R2 = $0001
+  dec r2 // R2--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r2, #$0000 // R2 = $0000
+  dec r2 // R2--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r3, #$0001 // R3 = $0001
+  dec r3 // R3--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r3, #$0000 // R3 = $0000
+  dec r3 // R3--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r4, #$0001 // R4 = $0001
+  dec r4 // R4--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r4, #$0000 // R4 = $0000
+  dec r4 // R4--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r5, #$0001 // R5 = $0001
+  dec r5 // R5--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r5, #$0000 // R5 = $0000
+  dec r5 // R5--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r6, #$0001 // R6 = $0001
+  dec r6 // R6--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r6, #$0000 // R6 = $0000
+  dec r6 // R6--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r7, #$0001 // R7 = $0001
+  dec r7 // R7--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r7, #$0000 // R7 = $0000
+  dec r7 // R7--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r8, #$0001 // R8 = $0001
+  dec r8 // R8--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r8, #$0000 // R8 = $0000
+  dec r8 // R8--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r9, #$0001 // R9 = $0001
+  dec r9 // R9--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r9, #$0000 // R9 = $0000
+  dec r9 // R9--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r10, #$0001 // R10 = $0001
+  dec r10 // R10--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r10, #$0000 // R10 = $0000
+  dec r10 // R10--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r11, #$0001 // R11 = $0001
+  dec r11 // R11--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r11, #$0000 // R11 = $0000
+  dec r11 // R11--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r12, #$0001 // R12 = $0001
+  dec r12 // R12--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r12, #$0000 // R12 = $0000
+  dec r12 // R12--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r13, #$0001 // R13 = $0001
+  dec r13 // R13--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r13, #$0000 // R13 = $0000
+  dec r13 // R13--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r14, #$0001 // R14 = $0001
+  dec r14 // R14--
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r14, #$0000 // R14 = $0000
+  dec r14 // R14--
+
+  stop // Stop GSU
+  nop // Delay Slot
---
+++ CHIP/GSU/GSUTest/DEC/make.bat
@@ -0,0 +1,1 @@
+bass GSUDEC.asm
