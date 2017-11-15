18e651fa86a7ae4b5ac4b02a9299d93b31cfc4d5
PeterLemon <krom@gbadev.org>
2017-11-15 03:25:33 +0000

    GSU INC Test

---
+++ CHIP/GSU/GSUTest/INC/GSUINC_gsu.asm
@@ -0,0 +1,187 @@
+// SNES GSU Test INC (Increment) demo (GSU Code) by krom (Peter Lemon):
+arch snes.gsu
+
+GSUStart:
+  ////////////////////////////
+  // INC register
+  ////////////////////////////
+
+  iwt r0, #$FFFF // R0 = $FFFF
+  inc r0 // R0++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r0, #$FFFE // R0 = $FFFE
+  inc r0 // R0++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r1, #$FFFF // R1 = $FFFF
+  inc r1 // R1++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r1, #$FFFE // R1 = $FFFE
+  inc r1 // R1++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r2, #$FFFF // R2 = $FFFF
+  inc r2 // R2++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r2, #$FFFE // R2 = $FFFE
+  inc r2 // R2++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r3, #$FFFF // R3 = $FFFF
+  inc r3 // R3++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r3, #$FFFE // R3 = $FFFE
+  inc r3 // R3++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r4, #$FFFF // R4 = $FFFF
+  inc r4 // R4++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r4, #$FFFE // R4 = $FFFE
+  inc r4 // R4++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r5, #$FFFF // R5 = $FFFF
+  inc r5 // R5++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r5, #$FFFE // R5 = $FFFE
+  inc r5 // R5++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r6, #$FFFF // R6 = $FFFF
+  inc r6 // R6++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r6, #$FFFE // R6 = $FFFE
+  inc r6 // R6++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r7, #$FFFF // R7 = $FFFF
+  inc r7 // R7++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r7, #$FFFE // R7 = $FFFE
+  inc r7 // R7++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r8, #$FFFF // R8 = $FFFF
+  inc r8 // R8++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r8, #$FFFE // R8 = $FFFE
+  inc r8 // R8++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r9, #$FFFF // R9 = $FFFF
+  inc r9 // R9++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r9, #$FFFE // R9 = $FFFE
+  inc r9 // R9++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r10, #$FFFF // R10 = $FFFF
+  inc r10 // R10++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r10, #$FFFE // R10 = $FFFE
+  inc r10 // R10++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r11, #$FFFF // R11 = $FFFF
+  inc r11 // R11++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r11, #$FFFE // R11 = $FFFE
+  inc r11 // R11++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r12, #$FFFF // R12 = $FFFF
+  inc r12 // R12++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r12, #$FFFE // R12 = $FFFE
+  inc r12 // R12++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r13, #$FFFF // R13 = $FFFF
+  inc r13 // R13++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r13, #$FFFE // R13 = $FFFE
+  inc r13 // R13++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r14, #$FFFF // R14 = $FFFF
+  inc r14 // R14++
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r14, #$FFFE // R14 = $FFFE
+  inc r14 // R14++
+
+  stop // Stop GSU
+  nop // Delay Slot
---
+++ CHIP/GSU/GSUTest/INC/make.bat
@@ -0,0 +1,1 @@
+bass GSUINC.asm
