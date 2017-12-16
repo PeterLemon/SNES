44a662f899f81f2d139de8d2d6d64f69b4ba278f
PeterLemon <krom@gbadev.org>
2017-12-16 23:45:04 +0000

    GSU IWT Test

---
+++ CHIP/GSU/GSUTest/IWT/GSUIWT_gsu.asm
@@ -0,0 +1,187 @@
+// SNES GSU Test IWT (Immediate Word Transfer) demo (GSU Code) by krom (Peter Lemon):
+arch snes.gsu
+
+GSUStart:
+  ////////////////////////////
+  // IWT register, #nnnn
+  ////////////////////////////
+
+  iwt r0, #$0000 // R0 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r0, #$FFFF // R0 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r1, #$0000 // R1 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r1, #$FFFF // R1 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r2, #$0000 // R2 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r2, #$FFFF // R2 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r3, #$0000 // R3 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r3, #$FFFF // R3 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r4, #$0000 // R4 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r4, #$FFFF // R4 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r5, #$0000 // R5 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r5, #$FFFF // R5 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r6, #$0000 // R6 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r6, #$FFFF // R6 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r7, #$0000 // R7 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r7, #$FFFF // R7 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r8, #$0000 // R8 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r8, #$FFFF // R8 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r9, #$0000 // R9 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r9, #$FFFF // R9 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r10, #$0000 // R10 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r10, #$FFFF // R10 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r11, #$0000 // R11 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r11, #$FFFF // R11 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r12, #$0000 // R12 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r12, #$FFFF // R12 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r13, #$0000 // R13 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r13, #$FFFF // R13 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r14, #$0000 // R14 = $0000
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  iwt r14, #$FFFF // R14 = $FFFF
+  nop // Delay Slot
+
+  stop // Stop GSU
+  nop // Delay Slot
---
+++ CHIP/GSU/GSUTest/IWT/make.bat
@@ -0,0 +1,1 @@
+bass GSUIWT.asm
