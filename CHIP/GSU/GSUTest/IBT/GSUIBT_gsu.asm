e9f05f9143bd0091ea5866f80590b78076fbdb41
PeterLemon <krom@gbadev.org>
2017-12-16 22:06:47 +0000

    GSU IBT Test

---
+++ CHIP/GSU/GSUTest/IBT/GSUIBT_gsu.asm
@@ -0,0 +1,217 @@
+// SNES GSU Test IBT (Immediate Byte Transfer) demo (GSU Code) by krom (Peter Lemon):
+arch snes.gsu
+
+GSUStart:
+  ////////////////////////////
+  // IBT register, #nn
+  ////////////////////////////
+
+  ibt r0, #$7F // R0 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r0, #$FF // R0 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r1, #$7F // R1 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r1, #$FF // R1 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r2, #$7F // R2 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r2, #$FF // R2 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r3, #$7F // R3 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r3, #$FF // R3 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r4, #$7F // R4 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r4, #$FF // R4 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r5, #$7F // R5 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r5, #$FF // R5 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r6, #$7F // R6 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r6, #$FF // R6 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r7, #$7F // R7 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r7, #$FF // R7 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r8, #$7F // R8 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r8, #$FF // R8 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r9, #$7F // R9 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r9, #$FF // R9 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r10, #$7F // R10 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r10, #$FF // R10 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r11, #$7F // R11 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r11, #$FF // R11 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r12, #$7F // R12 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r12, #$FF // R12 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r13, #$7F // R13 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r13, #$FF // R13 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r14, #$7F // R14 = $007F
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
+
+  ibt r14, #$FF // R14 = $FFFF
+  nop
+  nop
+
+  stop // Stop GSU
+  nop // Delay Slot
---
+++ CHIP/GSU/GSUTest/IBT/make.bat
@@ -0,0 +1,1 @@
+bass GSUIBT.asm
