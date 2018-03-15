5cf10c8341861a34532cf408bee062c86be31187
PeterLemon <krom@gbadev.org>
2018-03-15 23:08:42 +0000

    Use Words In Red Space HDMA Demo

--- PPU/HDMA/RedSpaceHDMA/RedSpaceHDMA.asm
+++ PPU/HDMA/RedSpaceHDMA/RedSpaceHDMA.asm
@@ -35,36 +35,36 @@ Loop:
   jmp Loop
 
 HDMATable:
-  db $07, $00, $00, $1F, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color0
-  db $07, $00, $00, $1E, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color1
-  db $07, $00, $00, $1D, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color2
-  db $07, $00, $00, $1C, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color3
-  db $07, $00, $00, $1B, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color4
-  db $07, $00, $00, $1A, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color5
-  db $07, $00, $00, $19, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color6
-  db $07, $00, $00, $18, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color7
-  db $07, $00, $00, $17, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color8
-  db $07, $00, $00, $16, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color9
-  db $07, $00, $00, $15, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color10
-  db $07, $00, $00, $14, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color11
-  db $07, $00, $00, $13, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color12
-  db $07, $00, $00, $12, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color13
-  db $07, $00, $00, $11, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color14
-  db $07, $00, $00, $10, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color15
-  db $07, $00, $00, $0F, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color16
-  db $07, $00, $00, $0E, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color17
-  db $07, $00, $00, $0D, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color18
-  db $07, $00, $00, $0C, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color19
-  db $07, $00, $00, $0B, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color20
-  db $07, $00, $00, $0A, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color21
-  db $07, $00, $00, $09, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color22
-  db $07, $00, $00, $08, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color23
-  db $07, $00, $00, $07, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color24
-  db $07, $00, $00, $06, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color25
-  db $07, $00, $00, $05, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color26
-  db $07, $00, $00, $04, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color27
-  db $07, $00, $00, $03, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color28
-  db $07, $00, $00, $02, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color29
-  db $07, $00, $00, $01, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color30
-  db $07, $00, $00, $00, $00 // Repeat 7 Scanlines, Palette Address 0, Gradient Color31
+  db $07; dw $0000, $001F // Repeat 7 Scanlines, Palette Address 0, Gradient Color0
+  db $07; dw $0000, $001E // Repeat 7 Scanlines, Palette Address 0, Gradient Color1
+  db $07; dw $0000, $001D // Repeat 7 Scanlines, Palette Address 0, Gradient Color2
+  db $07; dw $0000, $001C // Repeat 7 Scanlines, Palette Address 0, Gradient Color3
+  db $07; dw $0000, $001B // Repeat 7 Scanlines, Palette Address 0, Gradient Color4
+  db $07; dw $0000, $001A // Repeat 7 Scanlines, Palette Address 0, Gradient Color5
+  db $07; dw $0000, $0019 // Repeat 7 Scanlines, Palette Address 0, Gradient Color6
+  db $07; dw $0000, $0018 // Repeat 7 Scanlines, Palette Address 0, Gradient Color7
+  db $07; dw $0000, $0017 // Repeat 7 Scanlines, Palette Address 0, Gradient Color8
+  db $07; dw $0000, $0016 // Repeat 7 Scanlines, Palette Address 0, Gradient Color9
+  db $07; dw $0000, $0015 // Repeat 7 Scanlines, Palette Address 0, Gradient Color10
+  db $07; dw $0000, $0014 // Repeat 7 Scanlines, Palette Address 0, Gradient Color11
+  db $07; dw $0000, $0013 // Repeat 7 Scanlines, Palette Address 0, Gradient Color12
+  db $07; dw $0000, $0012 // Repeat 7 Scanlines, Palette Address 0, Gradient Color13
+  db $07; dw $0000, $0011 // Repeat 7 Scanlines, Palette Address 0, Gradient Color14
+  db $07; dw $0000, $0010 // Repeat 7 Scanlines, Palette Address 0, Gradient Color15
+  db $07; dw $0000, $000F // Repeat 7 Scanlines, Palette Address 0, Gradient Color16
+  db $07; dw $0000, $000E // Repeat 7 Scanlines, Palette Address 0, Gradient Color17
+  db $07; dw $0000, $000D // Repeat 7 Scanlines, Palette Address 0, Gradient Color18
+  db $07; dw $0000, $000C // Repeat 7 Scanlines, Palette Address 0, Gradient Color19
+  db $07; dw $0000, $000B // Repeat 7 Scanlines, Palette Address 0, Gradient Color20
+  db $07; dw $0000, $000A // Repeat 7 Scanlines, Palette Address 0, Gradient Color21
+  db $07; dw $0000, $0009 // Repeat 7 Scanlines, Palette Address 0, Gradient Color22
+  db $07; dw $0000, $0008 // Repeat 7 Scanlines, Palette Address 0, Gradient Color23
+  db $07; dw $0000, $0007 // Repeat 7 Scanlines, Palette Address 0, Gradient Color24
+  db $07; dw $0000, $0006 // Repeat 7 Scanlines, Palette Address 0, Gradient Color25
+  db $07; dw $0000, $0005 // Repeat 7 Scanlines, Palette Address 0, Gradient Color26
+  db $07; dw $0000, $0004 // Repeat 7 Scanlines, Palette Address 0, Gradient Color27
+  db $07; dw $0000, $0003 // Repeat 7 Scanlines, Palette Address 0, Gradient Color28
+  db $07; dw $0000, $0002 // Repeat 7 Scanlines, Palette Address 0, Gradient Color29
+  db $07; dw $0000, $0001 // Repeat 7 Scanlines, Palette Address 0, Gradient Color30
+  db $07; dw $0000, $0000 // Repeat 7 Scanlines, Palette Address 0, Gradient Color31
   db $00 // End Of HDMA
