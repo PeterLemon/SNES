57eafd6df3766d6efff167098b6158dddfd5d2f3
peterlemon <krom@gbadev.org>
2019-04-17 02:27:30 +0100

    Title Screen Book Mode7 GFX

--- Translate/Tetris Battle Gaiden/Tetris Battle Gaiden.asm
+++ Translate/Tetris Battle Gaiden/Tetris Battle Gaiden.asm
@@ -1174,7 +1174,7 @@ origin $95112; insert "CharacterSelect.rle" // VRAM $0000..$3FFF 4BPP Character 
 origin $FD2B2; insert "CharacterSelectPageMAP.rle" // VRAM $F000..$F7FF MAP Character Select Page RLE (2307 Bytes)
 
 // Include Title Screen Book Tiles
-origin $9D06F; insert "TitleScreenBookMode7.rle" // VRAM $0001..$7FFF 8BPP Title Screen Book Mode7 RLE (10145 Bytes)
+origin $9D06F; insert "TitleScreenBookMode7.rle" // VRAM $0001..$7FFF 8BPP Title Screen Book Mode7 RLE (10149 Bytes)
 // Include Title Screen Tiles
 origin $D6125; insert "TitleScreen.rle" // VRAM $C000..$FFFF 4BPP Title Screen RLE (7248 Bytes)
 
